11d8eb07d746e3428b5d44c6ea3515c24efe7377
nathanthefriendlybabydev <nathancplaster@gmail.com>
2021-10-26 13:50:38 -0400

    OS Homework 1

---
+++ consumer.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+#include<fcntl.h>
+#include <sys/shm.h>
+#include<sys/stat.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+
+int main(){
+    const char * s1 = "fill";
+    const char * s2 = "available";
+    const char * s3 = "mutex";
+
+    int tbl = shm_open("table", O_RDWR, 0666);
+    int * table = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, tbl, 0);
+    
+    int loop = 6;
+
+    sem_t * fill = sem_open(s1, O_CREAT | O_RDWR, 0666, 0);
+    if(fill == SEM_FAILED)
+      printf("fill");
+    sem_t * avail = sem_open(s2, O_CREAT | O_RDWR, 0666, 2);
+    if(avail == SEM_FAILED)
+      printf("avail");
+    sem_t * mutex = sem_open(s3, O_CREAT | O_RDWR, 0666, 1);
+    if(mutex == SEM_FAILED)
+      printf("mutex");
+
+    /*printf("\nConsumer ready to receive %d items.\n", loop);*/
+
+    for(int i=1; i<loop; ++i){
+        sem_wait(fill);
+
+        int ms = rand() % 2+i;
+        sleep(ms);
+
+        sem_wait(mutex);
+        --( * table);
+        sem_post(mutex);
+
+        printf("Item consumed, %d remaining.\n",* table);
+
+        sem_post(avail);
+        sem_post(fill);
+
+    }
+    sem_close(fill);
+    sem_close(avail);
+    sem_close(mutex);
+    sem_unlink(s1);
+    sem_unlink(s2);
+    sem_unlink(s3);
+
+    munmap(table,sizeof(int));
+    shm_unlink("table");
+    printf("Consumer went home!\n");
+    return 0;
+
+}
---
+++ producer.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+int main()
+{
+    const char * s1 = "fill";
+    const char * s2 = "available";
+    const char * s3 = "mutex";
+    int loop = 6;
+
+    sem_unlink(s1);
+    sem_unlink(s2);
+    sem_unlink(s3);
+
+    int tbl = shm_open("table", O_CREAT | O_RDWR, 0666);
+    
+    ftruncate(tbl,sizeof(int));
+    
+    int * table = mmap(0,sizeof(int), O_RDWR, MAP_SHARED, tbl, 0);
+    
+    sem_t* fill = sem_open(s1, O_CREAT | O_RDWR,0666,0);
+    if(fill == SEM_FAILED){
+      printf("fill failed");
+    }
+
+    sem_t* avail = sem_open(s2, O_CREAT | O_RDWR, 0666, 2);
+    if(avail == SEM_FAILED){
+      printf("avail failed");
+    }
+
+    sem_t* mutex = sem_open(s3,O_CREAT | O_RDWR,0666,1);
+    if(mutex == SEM_FAILED){
+      printf("mutex");
+    }
+
+  
+
+    printf("\nProducer ready to create %d items.\n", loop);
+
+        for(int i = 1; i < loop; ++i){
+        sem_wait(avail);
+
+        int ms=rand() % 2+i;
+        sleep(ms);
+        sem_wait(mutex);
+        ++(* table);
+        /*printf("Table items: %d",* table);*/
+        sem_post(mutex);
+        printf("Item produced, there are now %d item(s) in the table.\n",* table);
+        sem_post(fill);
+    }
+    
+    sem_close(fill);
+    sem_close(avail);
+    sem_close(mutex);
+    sem_unlink(s1);
+    sem_unlink(s2);
+    sem_unlink(s3);
+    munmap(table, sizeof(int));
+    close(tbl);
+    shm_unlink("table");
+    printf("Producer cleaned up!\n");
+    return 0;
+}
